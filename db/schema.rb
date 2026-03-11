47d8218c267ed129ca561f00b4b9ebc9cc90db45
Ruben <sneexy@disroot.org>
2026-03-11 11:35:06 -0500

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[8.0].define(version: 2026_02_03_172020) do
-=======
 ActiveRecord::Schema[8.1].define(version: 2026_03_10_095021) do
->>>>>>> upstream/main
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgroonga"
