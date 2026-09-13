2274a0143c6ba13870fba83ef73057918b7abade
Essem <smswessem@gmail.com>
2026-09-13 18:47:28 -0500

    Apply redesign changes to bubble feed

---
+++ app/javascript/flavours/glitch/features/bubble_timeline/components/feed_column_settings.tsx
@@ -0,0 +1,69 @@
+import { useCallback } from 'react';
+
+import { FormattedMessage } from 'react-intl';
+
+import type {
+  List as ImmutableList,
+  Map as ImmutableMap,
+  Record as ImmutableRecord,
+} from 'immutable';
+
+import { changeColumnParams } from '@/flavours/glitch/actions/columns';
+import { MenuItemCheckbox } from '@/flavours/glitch/components/menu';
+import type { MenuItemCheckboxChangeHandler } from '@/flavours/glitch/components/menu/items';
+import { useAppSelector, useAppDispatch } from 'flavours/glitch/store';
+
+import { changeSetting } from '../../../actions/settings';
+
+type Columns = ImmutableList<
+  ImmutableRecord<{
+    uuid: string;
+    params?: string;
+  }>
+>;
+
+export function useColumnSettings(columnId: string | undefined) {
+  return useAppSelector((state) => {
+    const columns = state.settings.get('columns') as Columns;
+    const index = columns.findIndex((c) => c.get('uuid') === columnId);
+
+    return columnId && index >= 0
+      ? columns.get(index)?.get('params')
+      : state.settings.get('bubble');
+  }) as ImmutableMap<string, unknown>;
+}
+
+export const FeedColumnSettings: React.FC<{
+  columnId: string | undefined;
+}> = ({ columnId }) => {
+  const dispatch = useAppDispatch();
+
+  const settings = useColumnSettings(columnId);
+
+  const onChange = useCallback<MenuItemCheckboxChangeHandler>(
+    ({ value, checked }) => {
+      if (columnId) {
+        dispatch(changeColumnParams(columnId, ['other', value], checked));
+      } else {
+        dispatch(changeSetting(['bubble', 'other', value], checked));
+      }
+    },
+    [columnId, dispatch],
+  );
+
+  const onlyMedia = settings.getIn(['other', 'onlyMedia']) as boolean;
+
+  return (
+    <MenuItemCheckbox
+      value='onlyMedia'
+      checked={onlyMedia}
+      onChange={onChange}
+      keepMenuOpenOnClick
+    >
+      <FormattedMessage
+        id='community.column_settings.media_only'
+        defaultMessage='Media only'
+      />
+    </MenuItemCheckbox>
+  );
+};
