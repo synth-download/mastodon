cf7b2cd7a2c5e52575e47bdd60c4d37909839ec4
Dmytro Oliinyk <dima@dol.ch>
2026-09-24 17:41:28 +0000

    [Glitch] Refactor compose path detection into a reusable utility function and add related tests
    Port e5fc481ab6685bf091b6c800b7ccff9e0e78087f to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/actions/compose.js
+++ app/javascript/flavours/glitch/actions/compose.js
@@ -11,6 +11,7 @@ import { emojiMartSearch } from '@/flavours/glitch/features/emoji/picker';
 import { recoverHashtags } from 'flavours/glitch/utils/hashtag';
 
 import { showAlert, showAlertForError } from './alerts';
+import { isStandaloneComposePath } from './compose_path';
 import { emojiUse } from './emojis';
 import { importFetchedAccounts, importFetchedStatus } from './importer';
 import { openModal } from './modal';
@@ -289,9 +290,7 @@ export function submitCompose(successCallback, overridePrivacy) {
         'Idempotency-Key': getState().getIn(['compose', 'idempotencyKey']),
       },
     }).then(function (response) {
-      if ((browserHistory.location.pathname === '/publish' || browserHistory.location.pathname === '/statuses/new')
-          && window.history.state
-          && !getState().getIn(['compose', 'advanced_options', 'threaded_mode'])) {
+      if (isStandaloneComposePath(browserHistory.location.pathname) && window.history.state && !getState().getIn(['compose', 'advanced_options', 'threaded_mode'])) {
         browserHistory.goBack();
       }
 
