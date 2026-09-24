26a23a15bd00a50caad84297e46592464b1a9d3a
diondiondion <mail@diondiondion.com>
2026-09-24 09:31:18 +0000

    [Glitch] Redesign: Move server announcements to notifications page
    Port 4b160e3811e64552a1c97966b518bc2cd2cbc6db to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/navigation_panel/redesign/index.tsx
+++ app/javascript/flavours/glitch/features/navigation_panel/redesign/index.tsx
@@ -29,6 +29,8 @@ import { useAppDispatch, useAppSelector } from '@/flavours/glitch/store';
 import { invokeVirtualIosKeyboard } from '@/flavours/glitch/utils/invoke_virtual_ios_keyboard';
 import FediIcon from '@/images/icons/icon_fediverse.svg?react';
 
+import { useHasAnnouncements } from '../../announcements/hooks';
+
 import { NavigationAccountCardAndMenu } from './account_card_and_menu';
 import { NavigationFooterLinks } from './footer_links';
 import { NavigationHeader } from './header';
@@ -76,6 +78,15 @@ function useFollowedHashtags() {
   return { followedHashtags: tags };
 }
 
+export function useNotificationsCount() {
+  const unreadNotificationsCount = useAppSelector(
+    selectUnreadNotificationGroupsCount,
+  );
+  const { unreadAnnouncementCount } = useHasAnnouncements();
+
+  return unreadNotificationsCount + unreadAnnouncementCount;
+}
+
 const isFediverseFeedsLinkActive = (
   match: unknown,
   { pathname }: { pathname: string },
@@ -97,9 +108,7 @@ export const RedesignNavigationPanel: React.FC<{
   const intl = useIntl();
   const dispatch = useAppDispatch();
   const { signedIn } = useIdentity();
-  const notificationsCount = useAppSelector(
-    selectUnreadNotificationGroupsCount,
-  );
+  const notificationsCount = useNotificationsCount();
 
   const openComposer = useCallback(() => {
     dispatch(closeNavigation());
